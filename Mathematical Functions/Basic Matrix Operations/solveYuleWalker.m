a6c59a22f47d5d5d9eff1e5c62c7cefd0df3ada3
David Crouse <croused@croused-x-wks.radar5300.nrl.navy.mil>
2016-12-06 10:38:08 -0500

    Corrected bugs in the Yule-Walker code.

--- Mathematical Functions/Basic Matrix Operations/solveYuleWalker.m
+++ Mathematical Functions/Basic Matrix Operations/solveYuleWalker.m
@@ -1,36 +1,56 @@
 function y=solveYuleWalker(r,b)
-%%SOLVEYULEWALKER Solve the Yule-Walker equation or the general Yule-Walker
+%%SOLVEYULEWALKER Solve the Yule-Walker equation (for real values) as
+%          formulated using
+%          correlation coefficients or the general Yule-Walker
 %          equation. For the Yule-Walker equation, given a real positive
-%          definite Topeplitz matrix T=toeplitz([1;r(1:n-1)]), solve the
-%          system T*y=-r(n-1:-1:1). For the general Yule-Walker equation,
-%          given a real Toeplitz matrix T=toeplitz([1;r(1:n-1)]), solve the
-%          system T*y=b.
+%          definite Topeplitz matrix T=toeplitz([r(1:(n-1))]), solve the
+%          system T*y=-r(2:n). For the general Yule-Walker equation,
+%          given a real Toeplitz matrix T=toeplitz(r(1:(n-1))), solve
+%          the system T*y=b.
 %
 %INPUTS: r When solving the Yule-Walker equation, r is a real nX1 or 1Xn
 %          vector defining a Toeplitz matrix such that
 %          toeplitz([1;r(1:n-1)]) is positive definite. When solving the
 %          general Yule-Walker equation, r is an (n-1)X1 or 1X(n-1) vector
-%          with similar requirements for the toeplitz matrix.
+%          with similar requirements for the Toeplitz matrix.
 %        b If this parameter is provided and is not an empty matrix, then
-%          the system being solved is T*y=b with T=toeplitz([1;r(1:n-1)]).
-%          Otherwise, the system being solved is T*y=-r(n-1:-1:1).
+%          the system being solved is T*y=b with T=toeplitz(r).
 %
-%OUTPUTS: y The real solution to the system T*y=-r(n-1:-1:1).
+%OUTPUTS: y The real solution to the system T*y=-r(:) or T*y=b.
 %
 %If b is not provided, this uses Durbin's algorithm, which is Algorithm
 %4.7.1 in Chapter 4.7.3 of [1]. If b is provided, this uses Levinson's
-%algorithm, which is Algorithm 4.7.2 in Chapter 4.7.4 of [1].
+%algorithm, which is Algorithm 4.7.2 in Chapter 4.7.4 of [1]. The
+%algorithms are provided assuming that r(1)=1. Thus, the first step of both
+%procedures is to scale everything and remove r(1).
+%
+%The Yule-Walker equation as solved here arises in parameter estimation for
+%autoregressive systems when parameterized with correlation coefficients.
+%This is described in Chapter 1.8 of [2]. Given samples of data, such
+%correlation coefficients might be found using the correlation function
+%with the 'unbiased' option, which is essentially what is done in an
+%example in Chapter 3.3. of [2]. For a parameterization in terms of raw
+%data and not correlation coefficients, consider the function
+%solveAutoRegressiveSys.
 %
 %REFERENCES:
 %[1] G. H. Golub and C. F. Van Loan, Matrix Computations, 4th ed.
 %    Baltimore: Johns Hopkins University Press, 2013.
+%[2] S. Haykin, Adaptive Filter Theory, 4th ed. Upper Saddle River, NJ:
+%    Prentice Hall, 2002.
 %
 %December 2015 David F. Crouse, Naval Research Laboratory, Washington D.C.
 %(UNCLASSIFIED) DISTRIBUTION STATEMENT A. Approved for public release.
 
+r=r(:);
+
+scale=r(1);
+r=r(2:end)/scale;
+
 if(nargin<2||isempty(b))
-    %The system is toeplitz([1;r(1:n-1)])*y=-r(n-1:-1:1). Use Durbin's
+    %The system is toeplitz(r(1:(end-1)))*y=-r(2:end) Use Durbin's
     %algorithm.
+    
     n=length(r); 
     %Allocate space
     y=zeros(n,1); 
@@ -65,7 +85,7 @@ else
         mu=(b(k+1)-r(1:k)'*x(k:-1:1))/beta;
         v(1:k)=x(1:k)+mu*y(k:-1:1);
         x(1:k+1)=[v(1:k);mu];
-        if k<n-1
+        if(k<n-1)
             alpha=-(r(k+1)+r(1:k)'*y(k:-1:1))/beta;
             z(1:k)=y(1:k)+alpha*y(k:-1:1);
             y(1:k+1)=[z(1:k);alpha];
